f2a9892915ef8c5ad279e02032d0bdff6bece45c
Katie Truong <khntruong@ucdavis.edu>
2019-04-03 14:19:08 -0700

    First four prompts

--- src/recursion.js
+++ src/recursion.js
@@ -7,20 +7,52 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  // Base case
+  if (n == 0) {
+    return 1
+  } else if (n < 0) {
+    return null
+  } else {
+    return n * factorial(n - 1)
+  }
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length == 0) {
+    return 0
+  } else if (array.length == 1) {
+    return array[0]
+  } else {
+    return array[0] + sum(array.slice(1))
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      sum += arraySum(array[i])
+    } else {
+      sum += array[i]
+    }
+  }
+  return sum
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  n = Math.abs(n)
+  if (n == 0) {
+    return true 
+  } else if (n == 1) {
+    return false
+  } else {
+    return isEven(n-2)
+  }
 };
 
 // 5. Sum all integers below a given integer.
